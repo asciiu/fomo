5277a8c46a7a4908a2580122fa2a33218225e659
Flowmaster <ellyssin.gimhae@gmail.com>
2017-11-09 22:35:05 -0700

    The parser spec should do a useful test

--- backend/src/test/scala/com/flow/marketmaker/models/ConditionParserSpec.scala
+++ backend/src/test/scala/com/flow/marketmaker/models/ConditionParserSpec.scala
@@ -8,13 +8,13 @@ class ConditionParserSpec extends FlatSpec with Matchers {
   val conditionA = "(price >= 0.00005000) || trailingStopLoss(0.05)"
   val conditionB = "(price <= 0.00045)"
 
-  "ConditionParser" should "parse price conditions" in {
+  "Evaluating a condition" should "return a boolean" in {
     val condition = "(price >= 0.00045)"
     val price = 0.5
-    val condition = s"($price >= 0.4 && $price <= 0.6)"
-    val tb = universe.runtimeMirror(getClass.getClassLoader).mkToolBox()
-    tb.eval(tb.parse(s"println($condition)"))
 
-    true should be(true)
+    val tb = universe.runtimeMirror(getClass.getClassLoader).mkToolBox()
+    val result = tb.eval(tb.parse(condition.replace("price", price.toString))) == true
+
+    result should be(true)
   }
 }
