f697e368734f675ae7b5d2396de4a3c337a70390
Flowmaster <ellyssin.gimhae@gmail.com>
2017-11-29 23:23:11 -0700

    Added index.html route for health check

--- api/src/main/scala/com/flowy/fomoapi/Routes.scala
+++ api/src/main/scala/com/flowy/fomoapi/Routes.scala
@@ -7,6 +7,7 @@ import com.softwaremill.bootzooka.ServerConfig
 import com.softwaremill.bootzooka.common.api.RoutesRequestWrapper
 import com.softwaremill.bootzooka.swagger.SwaggerDocService
 
+
 trait Routes extends RoutesRequestWrapper
   with UsersRoutes
   with PasswordResetRoutes
@@ -23,12 +24,22 @@ trait Routes extends RoutesRequestWrapper
       versionRoutes ~
       tradeRoutes
     } ~
+    indexRoute ~
     swaggerDocs
   }
+
+  // needed for aws health checks
+  def indexRoute =
+    path("index.html") {
+      get {
+        completeOk
+      }
+    }
 
   // swagger for api docs
   def swaggerDocs = path("docs") {
     getFromResource("swagger/index.html") } ~
     new SwaggerDocService(config.serverHost, config.serverPort, system).routes ~
     getFromResourceDirectory("swagger")
+
 }
