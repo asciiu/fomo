5713e1c400dc03be58d7f742129e2d0cc3143246
Flowmaster <ellyssin.gimhae@gmail.com>
2017-12-21 22:34:28 -0700

    The TradeActor shall receive updates from its parent

--- bittrex-exchange/src/main/scala/com/flowy/bexchange/MarketTradeService.scala
+++ bittrex-exchange/src/main/scala/com/flowy/bexchange/MarketTradeService.scala
@@ -43,9 +43,8 @@ class MarketTradeService(val marketName: String, bagel: TheEverythingBagelDao, r
 
   implicit val akkaSystem = context.system
 
-  case class TradeBuyCondition(tradeId: UUID, conditionEx: String)
-
-  val trades = collection.mutable.ListBuffer[Trade]()
+  // maps id of trade to the trade actor
+  val trades = collection.mutable.Map[UUID, ActorRef]()
 
   val dynamic = currentMirror.mkToolBox()
 
@@ -55,7 +54,9 @@ class MarketTradeService(val marketName: String, bagel: TheEverythingBagelDao, r
   override def preStart() = {
     // load pending conditions from bagel
     bagel.findTradesByStatus(marketName, TradeStatus.Pending).map { pendingTrades =>
-      trades ++= pendingTrades
+      pendingTrades.foreach { trade =>
+        trades += trade.id -> context.actorOf(TradeActor.props(trade, bagel), trade.id.toString)
+      }
     }
 
     log.info(s"$marketName actor started")
@@ -63,7 +64,8 @@ class MarketTradeService(val marketName: String, bagel: TheEverythingBagelDao, r
 
   def receive: Receive = {
     case update: MarketUpdate =>
-      updateState(update)
+      // forward to all child actors of me
+      context.system.actorSelection(s"${self.path}/*") ! update
 
     case PostTrade(user, request, Some(sender)) =>
       postTrade(user, request, sender)
@@ -78,97 +80,6 @@ class MarketTradeService(val marketName: String, bagel: TheEverythingBagelDao, r
       log.warning(s"received unknown message - $x")
   }
 
-  private def executeBuyTrades(lastPrice: Double) = {
-    val buyTrades = trades.filter { trade =>
-      val condition = trade.buyConditions.replace("price", lastPrice.toString)
-      dynamic.eval(dynamic.parse(s"$condition")) == true
-    }.filter { trade =>
-      trade.status == TradeStatus.Pending
-    }
-
-    buyTrades.foreach { trade =>
-      // TODO this is where the order shall be executed via the BittrexClient
-
-      // TODO the buyPrice should be the actual price you may need to read this from bittrex
-      log.info(s"buy ${trade.baseQuantity} ${trade.info.marketName} for user: ${trade.userId}")
-      val updatedTrade = trade.copy(
-        stat = TradeStat(
-          buyPrice = Some(lastPrice),
-          buyTime = Some(Instant.now().atOffset(ZoneOffset.UTC))),
-        status = TradeStatus.Bought
-      )
-
-      // TODO don't assume this update succeeded
-      bagel.updateTrade(updatedTrade)
-      trades -= trade
-
-      trade.stopLossConditions.map { sellConditions =>
-        sellConditions.split(" or ").foreach { c =>
-          if (c.contains("TrailingStop")) {
-            // TODO regex pattern for simple price conditions
-            val extractParams =
-              """^.*?TrailingStop\((0\.\d{2}),\s(\d+\.\d+).*?""".r
-            c match {
-              case extractParams(percent, refPrice) =>
-                val trailStop = TrailingStop(trade.userId, trade.id, trade.info.marketName, percent.toDouble, refPrice.toDouble)
-                mediator ! Publish("TrailingStop", TrailingStop(trade.userId, trade.id, trade.info.marketName, percent.toDouble, refPrice.toDouble))
-                log.info(s"sending trailing stop $trailStop")
-              case _ =>
-                log.warning(s"Leo is sent in an invalid format for the TrailingStop got: $c should be TrailingStop(percent, startTopPriceAt)")
-            }
-          } else {
-            // assume simple conditions here
-            trades.append(updatedTrade)
-          }
-        }
-
-        // no need to return anything from this map
-        Unit
-      }
-    }
-  }
-
-  private def executeSellTrades(lastPrice: Double) = {
-    val sellTrades = trades.filter { trade =>
-      val stopLoss = trade.stopLossConditions.getOrElse("").replace("price", lastPrice.toString)
-      val stopProfit = trade.takeProfitConditions.getOrElse("").replace("price", lastPrice.toString)
-
-      (stopLoss != "" && dynamic.eval(dynamic.parse(s"$stopLoss")) == true) ||
-      (stopProfit != "" && dynamic.eval(dynamic.parse(s"$stopProfit")) == true)
-    }.filter { trade =>
-      trade.status == TradeStatus.Bought
-    }
-
-    sellTrades.foreach { trade =>
-      // TODO this is where the order shall be executed via the BittrexClient
-
-      // TODO the sellPrice should be the actual price you may need to read this from bittrex
-      log.info(s"sell ${trade.info.marketName} for user: ${trade.userId}")
-      val updatedTrade = trade.copy(
-        stat = TradeStat(
-          sellPrice = Some(lastPrice),
-          sellTime = Some(Instant.now().atOffset(ZoneOffset.UTC))),
-        status = TradeStatus.Sold
-      )
-
-      // TODO don't assume this update succeeded
-      bagel.updateTrade(updatedTrade)
-      trades -= trade
-
-      // TODO may need to communicate this to cancel trailing stop
-      //mediator ! Publish("CancelTrailingStop", TrailingStop(trade.userId, trade.id, trade.info.marketName))
-    }
-  }
-
-  /**
-    * Updates the internal state of this service.
-    * @param update
-    * @return
-    */
-  private def updateState(update: MarketUpdate) = {
-    Future(executeBuyTrades(update.Last))
-    Future(executeSellTrades(update.Last))
-  }
 
   /**
     * Add trade to DB and insert buy conditions.
@@ -194,7 +105,8 @@ class MarketTradeService(val marketName: String, bagel: TheEverythingBagelDao, r
         val newBalance = balance.copy(availableBalance = balance.availableBalance - trade.baseQuantity)
         bagel.updateBalance(newBalance)
 
-        trades.append(trade)
+        // start new process to watch this trade
+        trades += trade.id -> context.actorOf(TradeActor.props(trade, bagel), trade.id.toString)
 
         // send trade response to sender
         senderRef ! Some(trade)
@@ -204,31 +116,11 @@ class MarketTradeService(val marketName: String, bagel: TheEverythingBagelDao, r
   }
 
   private def deleteTrade(trade: Trade, sender: ActorRef) = {
-    // cannot cancel a sold trade because it is already finished.
-    trade.status match {
-      case TradeStatus.Sold =>
-        // cannot cancel a sold trade because it is already finished.
-        sender ! None
-
-      case TradeStatus.Pending =>
-        // delete the trade from the system if pending
-        for {
-          deletedOpt <- bagel.deleteTrade(trade)
-          balanceOpt <- bagel.findBalance(trade.userId, trade.apiKeyId, trade.info.baseCurrency)
-        } yield {
-
-          (deletedOpt, balanceOpt) match {
-            case (Some(deleted), Some(balance)) =>
-              bagel.updateBalance(balance.copy(availableBalance = balance.availableBalance + deleted.baseQuantity)).map ( _ => sender ! Some(deleted))
-
-            case _ =>
-              sender ! None
-          }
-        }
-
-      case _ =>
-        // all other trades statuses are cancellable
-        bagel.updateTrade(trade.copy(status = TradeStatus.Cancelled)).map (sender ! _)
+    if (trades.contains(trade.id)) {
+      context stop trades(trade.id)
+      sender ! Some(trade)
+    } else {
+      sender ! None
     }
   }
 
